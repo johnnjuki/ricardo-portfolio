f24e3d1bfe0d75c66bf4660758ef75441dc81b5b
John Njuki <njukijohn2000@gmail.com>
2023-08-15 00:04:37 +0300

    remove github and linkedin links

--- components/intro.tsx
+++ components/intro.tsx
@@ -96,15 +96,13 @@ export default function Intro() {
         </a>
         <a
           className=" flex cursor-pointer items-center gap-2 rounded-full borderBlack bg-white p-4 text-gray-700 transition hover:scale-[1.15] hover:text-gray-950 focus:scale-[1.15] active:scale-105 dark:bg-white/10 dark:text-white/60"
-          href="https://linkedin.com"
-          target="_blank"
+          href=""
         >
           <BsLinkedin />
         </a>
         <a
           className="flex cursor-pointer items-center gap-2 rounded-full borderBlack bg-white p-4 text-[1.35rem] text-gray-700 transition hover:scale-[1.15] hover:text-gray-950 focus:scale-[1.15] active:scale-105 dark:bg-white/10 dark:text-white/60"
-          href="https://github.com"
-          target="_blank"
+          href=""
         >
           <FaGithubSquare />
         </a>
